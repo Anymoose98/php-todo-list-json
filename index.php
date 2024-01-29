02fe2e0ad681b453e03b61121c294b68339ef5ba
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-29 21:39:13 +0100

    add bootstrap

--- index.php
+++ index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
-    <h1>prova</h1>
+    <h1 class="text-center">prova</h1>
 </body>
 </html>
