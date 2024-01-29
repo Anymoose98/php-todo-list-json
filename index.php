4be3801a5d69b607d5a203547c7337a9faf27668
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-29 21:55:32 +0100

    struttura base creata

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
-    <h1 class="text-center">prova</h1>
+   
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.7/axios.min.js" integrity="sha512-NQfB/bDaB8kaSXF8E77JjhHG5PM6XVRxvHzkZiwl3ddWCEPBa23T76MuWSwAJdMGJnmQqM0VeY9kFszsrBEFrQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 <script src="./js/script.js"></script>
 </body>
 </html>
